22733abb45a945427221bb14fa99fe9f467ccfd5
piyush21malhotra <101010021+piyush21malhotra@users.noreply.github.com>
2023-03-11 14:56:13 +0530

    commit#1

---
+++ script2.js
@@ -0,0 +1,76 @@
+const slides = document.querySelector(".owl-stage").children;
+const indicator = document.querySelector(".owl-dots");
+let index = 0;
+
+// create circle indicators
+function circleIndicator() {
+    for (let i = 0; i < slides.length; i++) {
+        const div = document.createElement("div");
+        div.setAttribute("onclick", "indicateSlide(this)")
+        div.id = i;
+        if (i == 0) {
+            div.className = "active";
+        }
+        indicator.appendChild(div);
+    }
+}
+
+circleIndicator();
+
+function indicateSlide(element) {
+    index = element.id;
+    changeSlide();
+    updateCircleIndicator();
+    resetTimer();
+}
+
+function updateCircleIndicator() {
+    for (let i = 0; i < indicator.children.length; i++) {
+        indicator.children[i].classList.remove("active");
+    }
+    indicator.children[index].classList.add("active");
+}
+
+function prevSlide() {
+    if (index == 0) {
+        index = slides.length - 1;
+    }
+    else {
+        index--;
+    }
+    changeSlide();
+}
+
+function nextSlide() {
+    if (index == slides.length - 1) {
+        index = 0;
+    }
+    else {
+        index++;
+    }
+    changeSlide();
+}
+
+function changeSlide() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+
+    slides[index].classList.add("active");
+}
+
+function resetTimer() {
+    // when click to indicator or controls button 
+    // stop timer 
+    clearInterval(timer);
+    // then started again timer
+    timer = setInterval(autoPlay, 4000);
+}
+
+
+function autoPlay() {
+    nextSlide();
+    updateCircleIndicator();
+}
+
+let timer = setInterval(autoPlay, 4000);
